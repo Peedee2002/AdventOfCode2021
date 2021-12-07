8f19df9e45763d1eaa8180967f9a1f0bb46f65be
Peter Derias <peter_derias@hotmail.com>
2021-12-07 18:38:40 +1100

    at some point i have to admit that writing stinky python is kinda fnu

---
+++ day_7/day7.py
@@ -0,0 +1,15 @@
+def func(num):
+    # return num # for p1
+    return int((num * (num + 1))/2)
+    
+
+if __name__ == '__main__':
+    with open("input.in") as f:
+        ls = [int(num) for num in f.readline().split(',')]
+    print( \
+        min( \
+            sum( \
+                func(abs(pos - num)) for num in ls \
+            ) for pos in range(min(ls), max(ls)) \
+        ) \
+    )
