205dc5220077e6b33bc838f9fdfee4f7692fdd4b
Peter Derias <peter_derias@hotmail.com>
2021-12-09 19:35:50 +1100

    indentation is for nerds

--- day_9/day9_2.py
+++ day_9/day9_2.py
@@ -4,19 +4,15 @@ def recursive_fill_basins(x, y, array):
     if array[x][y][0] == 9 or array[x][y][1]:
         return 0
     array[x][y] = (array[x][y][0], True)
-    total = 0
-    if  x - 1 >= 0:
-        total += recursive_fill_basins(x - 1, y, array)
-    if x + 1 < len(array):
-        total += recursive_fill_basins(x + 1, y, array)
-    if  y - 1 >= 0:
-        total += recursive_fill_basins(x, y - 1, array)
-    if y + 1 < len(array[0]):
-        total += recursive_fill_basins(x, y + 1, array)
-    return 1 + total
+    total = 1
+    total += recursive_fill_basins(x - 1, y, array) if x - 1 >= 0 else 0
+    total += recursive_fill_basins(x + 1, y, array) if x + 1 < len(array) else 0
+    total += recursive_fill_basins(x, y - 1, array) if y - 1 >= 0 else 0
+    total += recursive_fill_basins(x, y + 1, array) if y + 1 < len(array[0]) else 0
+    return total
 
 if __name__ == '__main__':
-    array = [[(int(value), False) for value in line[:-1]] for line in open('example.in')]
+    array = [[(int(value), False) for value in line[:-1]] for line in open('input.in')]
     totals = [recursive_fill_basins(x, y, array) for x in range(len(array)) for y in range(len(array[0]))]
 
     print(prod(sorted(totals)[-3:]))
