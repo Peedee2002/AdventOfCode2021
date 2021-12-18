a26f15316655290a6ce02d402fe886b7d905793c
Peter Derias <peter_derias@hotmail.com>
2021-12-18 21:47:34 +1100

    made it harder to read by using less lines of code

--- day_17/day17.py
+++ day_17/day17.py
@@ -1,36 +1,26 @@
 x_box = (244, 303)
 y_box = (-91, -54)
-
-def in_box(x, y):
-    return x_box[0] <= x <= x_box[1] and y_box[0] <= y <= y_box[1]  
-
+ 
 def simulate(vx, vy):
-    x = 0
-    y = 0
+    x, y = 0, 0
     landed = False
     heights = []
     while x <= x_box[1] and y >= y_box[0]:
         heights.append(y)
-        x += vx
-        y += vy
+        x , y = vx + x, vy + y 
         vx -= 1 if vx > 0 else 0
         vy -= 1
-        if in_box(x, y):
-            landed = True
+        landed = x_box[0] <= x <= x_box[1] and y_box[0] <= y <= y_box[1] if not landed else landed
     return landed, max(heights)
 
 
 if __name__ == '__main__':
-    test_vx = range(x_box[1] + 1)
-    # do as much as seems reasonable? this is dumb, but this is why engineering is better than science right
-    # note: this is dumb because i have no guarantee on the upper bound here
-    test_vy = range(y_box[0], (y_box[1] - y_box[0]) * 3)
     heights = []
-    for vx in test_vx:
-        for vy in test_vy:
+    for vx in range(x_box[1] + 1):
+        # note: this is dumb because i have no maths to guarantee on the upper bound here
+        for vy in range(y_box[0], (y_box[1] - y_box[0]) * 3):
             landed, height = simulate(vx, vy)
             if landed:
                 heights.append(height)
-
     print(max(heights))
     print(len(heights))
