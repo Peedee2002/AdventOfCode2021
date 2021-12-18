e492aa8fddf18b7e03035dd48253bad6d2cdecff
Peter Derias <peter_derias@hotmail.com>
2021-12-18 22:10:33 +1100

    made it harder to read by using less lines of code

--- day_17/day17.py
+++ day_17/day17.py
@@ -9,7 +9,5 @@ def simulate(vx, vy):
         x , y, vx, vy = vx + x, vy + y, vx - 1 if vx > 0 else vx, vy - 1
         landed = x_box[0] <= x <= x_box[1] and y_box[0] <= y <= y_box[1] if not landed else landed
     return landed, max(heights)
-if __name__ == '__main__':
-    # this shouldnt be allowed
-    heights = [simulate(vx, vy)[1] for vx in range(x_box[1] + 1) for vy in range(y_box[0], (y_box[1] - y_box[0]) * 3) if simulate(vx, vy)[0]]
-    print(max(heights), len(heights))
+heights = [simulate(vx, vy)[1] for vx in range(x_box[1] + 1) for vy in range(y_box[0], (y_box[1] - y_box[0]) * 3) if simulate(vx, vy)[0]]
+print(max(heights), len(heights))
